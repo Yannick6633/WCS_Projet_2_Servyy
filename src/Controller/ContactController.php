fbcee3e10682c641d3cac63c61c0a327d7c08cec
Geoffrey BEDLE <geoffrey.bedle@gmail.com>
2019-04-16 14:47:22 +0200

    Validation serveur fonctionnelle contactform

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -16,7 +16,30 @@ class ContactController extends AbstractController
 
     public function index()
     {
+        $errors = [];
 
-        return $this->twig->render('Contact/contact.html.twig');
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST['email'])) {
+                $errors['emailError'] = ' * L\'adresse mail est obligatoire';
+            } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors['emailError'] = ' * Adresse mail non valide';
+            }
+
+            if(empty($_POST['subject'])){
+                $errors['subjectError']=' * Veuillez renseigner le sujet';
+            } elseif (!preg_match("/^[a-zA-Z ]*$/",$_POST['subject'])){
+                $errors['subjectError']=' * Lettres et espaces autorisés seulement';
+            }
+
+            if(empty($_POST['message'])){
+                $errors['messageError']=' * Veuillez renseigner le sujet';
+            } elseif (!preg_match("/^[a-zA-Z ]*$/",$_POST['message'])){
+                $errors['messageError']=' * Lettres et espaces autorisés seulement';
+            }
+
+        }
+
+        return $this->twig->render('Contact/contact.html.twig', ['post' => $_POST,
+            'errors' => $errors]);
     }
 }
