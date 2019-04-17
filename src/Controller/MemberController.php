5c9422346082acdf44f7c2432c59f927e8af9115
Laure <laure.fraysse@gmail.com>
2019-04-17 15:41:07 +0200

    dynamisation checkboxes page membre

---
+++ src/Controller/MemberController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laure
+ * Date: 2019-04-16
+ * Time: 15:49
+ */
+
+namespace App\Controller;
+
+
+use App\Model\ServiceManager;
+use App\Model\UserManager;
+
+class MemberController extends AbstractController
+{
+    public function index()
+    {
+        $serviceManager = new ServiceManager();
+        $services = $serviceManager->selectAll();
+
+
+        return $this->twig->render('Member/member.html.twig',['services'=>$services,
+            'memberData'=>$userData]);
+    }
+}
+
